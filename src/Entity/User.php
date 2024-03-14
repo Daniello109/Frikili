5e33ac11bcada891fd06f56a99e379357c3e5e2c
DaNieLLo109 <102591706+Daniello109@users.noreply.github.com>
2024-03-14 17:17:30 +0100

    explicacion entity User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,5 +1,17 @@
 <?php
 
+/*  Para crear esta entidad, se ejecuta en la consola el comando php bin/console make:entity
+    Pregunta el nombre de la entidad que se quiere crear o modificar (User). Como la entidad existe, propone agregar campos nuevos : pregunta el tipo, longitud, si puede ser nulo y después propone otra agregación 
+    Las anotaciones @ORM\Entity y @ORM\Id, @ORM\GeneratedValue, @ORM\Column se utilizan para mapear la clase User a una tabla de la base de datos y definir sus propiedades.
+     Un usuario puede escribir muchos posts. Cada post solo puede estar escrito por un usuario, aunque este puede escribir muchos => OneToMany 
+    Para crear las relaciones, ejecutamos el cmd php bin/console make:entity User. Como cada usuario puede tener muchos posts, se abre la entidad usuario y va a hacer algunas preguntas : 
+     - El nombre del campo que se va a insertar (posts)   - OneTomMany  - COn que entidad está relacionado : Post  
+     - Dentro de Post cómo se va a llamar : user   
+     - Puede ser nulo (un post no debe no tener usuario) No  
+     - Si desaparece el usuario, se borran los posts huérfanos ? : sí
+*/
+
+
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -8,8 +20,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User implements UserInterface
-{
+class User implements UserInterface   // : Las anotaciones @ORM\Entity y @ORM\Id, @ORM\GeneratedValue, @ORM\Column se utilizan para mapear la clase User a una tabla de la base de datos y definir sus propiedades.
+{ 
     const REGISTRO_EXITOSO = 'Se ha registrado exitosamente';
 
     /**
@@ -17,12 +29,12 @@ class User implements UserInterface
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private $id;                                                        // las lineas superiores determinan que este ID sea un entero, una columna, y se genere de manera automática y autoincrementable
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      */
-    private $email;
+    private $email;                                                    // este será un string único, de longitud 180 char, una columna. Nota : el tipo texto no da límite de carácteres
 
     /**
      * @ORM\Column(type="json")
@@ -33,7 +45,7 @@ class User implements UserInterface
      * @var string The hashed password
      * @ORM\Column(type="string")
      */
-    private $password;
+    private $password;                                                  //En este caso, indica que esta propiedad almacena la contraseña del usuario de forma cifrada (hashed).                                                       
 
     /**
      * @ORM\Column(type="boolean")
@@ -45,12 +57,15 @@ class User implements UserInterface
      * @ORM\Column(type="string")
      */
     private $nombre;
-
-
-    /**
+    
+                                                                                    //  La anotación @ORM\OneToMany es una anotación de Doctrine ORM que se utiliza para establecer una relación de uno a muchos entre dos entidades en una base de datos relacional.
+    /**                                                                                            
      * @ORM\OneToMany(targetEntity="App\Entity\Comentarios", mappedBy="user")
      */
-    private $comentarios;
+                                                                                    //targetEntity="App\Entity\Comentarios": Este parámetro especifica la clase de la entidad objetivo con la que se está estableciendo la relación. En este caso, se refiere a la clase Comentarios en el espacio de nombres App\Entity.
+                                                                                    //mappedBy="user": Este parámetro especifica el nombre de la propiedad en la entidad objetivo (Comentarios) que actúa como el lado inverso de la relación. En este caso, se refiere a la propiedad user en la clase Comentarios, que probablemente sea una propiedad que hace referencia a la entidad User.
+    
+    private $comentarios;                                                            //La declaración private $comentarios; define una propiedad de clase $comentarios, que se utilizará para almacenar una colección de comentarios asociados a un usuario.
 
 
 
